6958a5aa44d818b3582e66aab243345d72f7a1dd
VirLg <19virlg@gmail.com>
2024-07-30 18:54:37 +0300

    add button in product cart

--- src/components/pages/products/ProductItem.tsx
+++ src/components/pages/products/ProductItem.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import { useState } from "react";
 import { MyProduct } from "./models";
 
 interface ProductProps {
   product: MyProduct;
 }
 
-const ProductItem = ({ product }: ProductProps) => (
-  <div>
-    <img src={product.image} alt={product.title} />
-    <div>{product.title}</div>
-    <div>{product.description}</div>
-    <div>{product.price}</div>
-  </div>
-);
+const ProductItem = ({ product }: ProductProps) => {
+  const [details, setDetails] = useState(false);
+  return (
+    <div>
+      <img src={product.image} className="w-1/12 " alt={product.title} />
+      <div>{product.title}</div>
+      <div>{product.price}</div>
+      <button
+        className="w-32 h-12  border-4 "
+        onClick={() => setDetails(!details)}>
+        Show Details
+      </button>
+      {details && <div>{product.description}</div>}
+    </div>
+  );
+};
 
 export default ProductItem;
