36a9f4127b3315cb8d6de290b465e8a68be35948
VirLg <19virlg@gmail.com>
2024-08-01 14:27:05 +0300

    fix useEffect

--- src/components/pages/products/ProductList.tsx
+++ src/components/pages/products/ProductList.tsx
@@ -7,18 +7,18 @@ import { MyProduct } from "./models";
 const ProductList = () => {
   const [data, setData] = useState<MyProduct[]>([]);
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getAllProducts();
-        console.log("response", response);
-        setData(response); // Оновлення стану з завантаженими даними
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
-
-    fetchData(); // Виклик асинхронної функції для завантаження даних
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await getAllProducts();
+      setData(response);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
   return (
     <div>
       {data.map((product) => (
